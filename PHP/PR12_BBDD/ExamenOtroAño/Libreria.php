6629a166795b1e4d8f1a8c333596c8a33c52b06f
Jorge <Jlean92@gmail.com>
2024-01-22 18:55:45 +0100

    Changes committed: 01/22/2024 18:55:44 from IBLAESZARC03466

--- PHP/PR12_BBDD/ExamenOtroAño/Libreria.php
+++ PHP/PR12_BBDD/ExamenOtroAño/Libreria.php
@@ -7,7 +7,12 @@
 </head>
 <body>
 	<center>
-	<form>
+		<?php
+			if (!isset($_POST['aceptar'])) {
+				
+			
+		?>
+	<form method="POST" action="">
 		<h2>LIBRERIA - JORGE LEÓN</h2>
 		<label>Titulo :</label>
 		<input type="text" name="title"><br>
@@ -18,23 +23,73 @@
 		<label>Precio ejemplar :</label>
 		<input type="text" name="price"><br>
 		<h2>Genero:</h2>
-		<input type="radio" name="radio" value="novela">
+		<input type="radio" name="radio" value="Novela">
 		<label>Novela</label><br>
-		<input type="radio" name="radio" value="cuento">
+		<input type="radio" name="radio" value="Cuento">
 		<label>Cuento</label><br>
-		<input type="radio" name="radio" value="poesia">
+		<input type="radio" name="radio" value="Poesia">
 		<label>Poesia</label><br>
 		<h2>OPERACIONES:</h2>
 		<input type="checkbox" name="save">
 		<label>Guardar datos en la base de datos</label><br>
-		<input type="checkbox" name="save">
+		<input type="checkbox" name="mostrar">
 		<label>Mostrar libros del genero seleccionado junto con el precio Total</label><br>
-		<input type="checkbox" name="save">
+		<input type="checkbox" name="contar">
 		<label>Cantidad de libros de cada genero junto con el promedio del precio total</label><br>
 
 		<input type="submit" name="aceptar" value="Aceptar">
 		<input type="reset" name="limpiar" value="Limpiar formulario">
 	</form>
+		<?php
+		}
+		else{
+			$Opcion = $_POST['radio'];
+			$title = $_POST['title'];
+			$author = $_POST['author'];
+			$cantidad = $_POST['cantity'];
+			$price = $_POST['price'];
+
+
+			require("Conexion.php");
+			$conexion = new mysqli($Servidor.":".$Puerto, $Usuario, $Contraseña, $BaseDeDatos);
+
+			if (isset($_POST['save'])) {
+				$SQL = "Insert INTO libros ( $title , $author , $cantidad , $precio , $Opcion )";
+				$resultado = $conexion -> query($SQL);
+				if ($resultado) {
+					echo "Se ha insertado correctamente";
+				}else{
+					echo "No se ha insertado correctamente.";
+				}
+			}
+			if (isset($_POST['mostrar'])) {
+				$SQL = "SELECT * FROM libros";
+				$resultado = $conexion -> query($SQL);
+				$contador = 0;
+				echo "<tr>";
+				foreach ($resultado as $key => $fila) {
+					foreach ($fila as $key => $value) {
+						if ($contador == 0) {
+							echo "<td>$key</td>";
+						}
+						$contador++;
+					}
+				}
+				echo "</tr>";
+
+				foreach ($resultado as $key => $fila) {
+					echo "<tr>";
+					foreach ($fila as $key => $value) {
+						echo "$value";
+					}
+					echo "</tr>";
+				}
+			}
+			if (isset($_POST['contar'])) {
+				$SQL = "SELECT * FROM libros";
+			}
+		}
+		?>
 	</center>
 </body>
 </html>
