d0b6bb84543d52b3b0aaddb55617e1582a6ead83
Jorge <Jlean92@gmail.com>
2024-01-22 20:02:39 +0100

    Changes committed: 01/22/2024 20:02:38 from IBLAESZARC03466

---
+++ CambiarPagina.ps1
@@ -0,0 +1,9 @@
+﻿cd "c:\xampp\htdocs\MiApp"
+$TODAY= date
+$Equipo= hostname
+git remote rm origin
+git remote add origin https://github.com/jlean92/MiApp.git
+git add .
+git commit -m "Changes committed: $TODAY from $Equipo"
+git push --set-upstream origin main
+git pull
