af104f505f77381399dd420ad9a3fc75c755bbc6
Jorge <Jlean92@gmail.com>
2024-01-22 18:36:03 +0100

    Changes committed: 01/22/2024 18:36:02 from IBLAESZARC03466

--- PHP/PR12_BBDD/ExamenOtroAño/Libreria.php
+++ PHP/PR12_BBDD/ExamenOtroAño/Libreria.php
@@ -8,20 +8,27 @@
 <body>
 	<form>
 		<label>Titulo :</label>
-		<input type="text" name="title">
+		<input type="text" name="title"><br>
 		<label>Autor :</label>
-		<input type="text" name="author">
+		<input type="text" name="author"><br>
 		<label>Cantidad ejemplares :</label>
-		<input type="text" name="cantity">
+		<input type="text" name="cantity"><br>
 		<label>Precio ejemplar :</label>
-		<input type="text" name="price">
+		<input type="text" name="price"><br>
 		<h2>Genero:</h2>
 		<input type="radio" name="radio" value="novela">
-		<label>Novela</label>
+		<label>Novela</label><br>
 		<input type="radio" name="radio" value="cuento">
-		<label>Cuento</label>
+		<label>Cuento</label><br>
 		<input type="radio" name="radio" value="poesia">
-		<label>Poesia</label>
+		<label>Poesia</label><br>
+		<h2>OPERACIONES:</h2>
+		<input type="checkbox" name="save">
+		<label>Guardar datos en la base de datos</label><br>
+		<input type="checkbox" name="save">
+		<label>Mostrar libros del genero seleccionado junto con el precio Total</label><br>
+		<input type="checkbox" name="save">
+		<label>Cantidad de libros de cada genero junto con el promedio del precio total</label><br>
 
 		<input type="submit" name="aceptar" value="Aceptar">
 		<input type="reset" name="limpiar" value="Limpiar formulario">
