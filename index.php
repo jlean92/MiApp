f04ca368f426438aabd067aa2ff821db3cc01c1b
Jorge <Jlean92@gmail.com>
2024-01-28 17:06:22 +0100

    Changes committed: 01/28/2024 17:06:21 from Jlean92Pc

--- index.php
+++ index.php
@@ -76,43 +76,38 @@
     }
     else{
         require("Contrasenas.php");
-        $conexion = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+        $conexion = new mysqli($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
         $conexion -> set_charset("utf8");
         
     // Se valida si se ha enviado información, con la función isset()
         if (!isset($_POST['username'], $_POST['password'])) {
-            // si no hay datos muestra error y re direccionar
-            header('Location: index.php');
+            echo "No se han introducido usuarios";
         }
     // evitar inyección sql
-    if ($stmt = $conexion->prepare('SELECT Userid,contrasena,FirstName FROM users WHERE username = ?')) {
-        // parámetros de enlace de la cadena s
-        $stmt->bind_param('s', $_POST['username']);
-        $stmt->execute();
-    }
-    // acá se valida si lo ingresado coincide con la base de datos
+    $SQL = "SELECT UserName, Contrasena FROM users";
 
-    $stmt->store_result();
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password,$FirstName);
-        $stmt->fetch();
-
-        // se confirma que la cuenta existe ahora validamos la contraseña
-        if ($_POST['password'] === $password) {
-            // la conexion sería exitosa, se crea la sesión
-            session_regenerate_id();
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['name'] = $_POST['username'];
-            $_SESSION['id'] = $id;
-            $_SESSION['FirstName'] = $FirstName;
-            header('Location: inicio.php');
+    $resultado = $conexion -> query($SQL);
+    $acceso = false;
+        foreach ($resultado as $key => $fila) {
+            if (strcasecmp($_POST['username'], $fila['UserName'])) {
+                if (strcasecmp($_POST['password'], $fila['Contrasena'])) {
+                    $acceso = true;
+                }
+            }
         }
+    if ($acceso) {
+        // la conexion sería exitosa, se crea la sesión
+        session_regenerate_id();
+        $_SESSION['loggedin'] = TRUE;
+        $_SESSION['name'] = $_POST['username'];
+        $_SESSION['id'] = $id;
+        $_SESSION['FirstName'] = $FirstName;
+        header('Location: inicio.php');
     } else {
         // usuario incorrecto
         header('Location: index.php');
     }
-    $stmt->close();
-    }
+    
     ?>
 </body>
 </html>
